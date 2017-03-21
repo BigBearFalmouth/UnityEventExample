978dae18c536ed9b0b2b140a7dca3ce6bddff244
McDonald <Brian.McDonald@falmouth.ac.uk>
2017-03-21 15:39:17 +0000

    First Project setup

---
+++ Assets/MyAssets/AIListener.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIListener : MonoBehaviour {
+
+    public Material successMaterial;
+
+    Renderer renderer;
+    public EventManager eventManager;
+    // Use this for initialization
+    void Start () {
+        renderer = GetComponent<Renderer>();
+        eventManager.AddCharacterArrivedDelegate(OnCharacterArrivedAtWaypoint);
+
+    }
+
+
+    void OnCharacterArrivedAtWaypoint()
+    {
+        renderer.material = successMaterial;
+    }
+}
---
+++ Assets/MyAssets/AIStatusUpdater.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIStatusUpdater : MonoBehaviour {
+
+    NavMeshAgent agent;
+    public EventManager eventManager;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            float distance = agent.remainingDistance;
+ 
+            if (agent.pathPending && distance != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0)
+            {
+                eventManager.NotifyCharacterArrived();
+            }
+    }
+}
